63c734695f29bf29edf451c4ca1f0a6527cd265e
alonso-estevam <alonso_estevam@outlook.com>
2022-04-19 18:06:12 -0300

    Adicionei os arquivos das fases 2 e 3

--- tchalla/tchalla-fase2.js
+++ tchalla/tchalla-fase2.js
@@ -1,21 +1,21 @@
 function confereResposta() {
     var respostaJogador = prompt("Digite a resposta");
-    var respostaCerta = "C";
+    var respostaCerta = "A";
     if (respostaJogador.toUpperCase() === respostaCerta) {
         alert("Parabéns! Você acertou na primeira tentativa! Ganhou 50xp");
-        window.location.replace('tchalla-fase2.html');
+        window.location.replace('tchalla-fase3.html');
     } else {
         alert("Que pena, você errou. Tente novamente.");
         var respostaJogador = prompt("Digite a resposta");
         if (respostaJogador.toUpperCase() === respostaCerta) {
             alert("Parabéns! Você acertou na segunda tentativa! Ganhou 30xp");
-            window.location.replace('tchalla-fase2.html');
+            window.location.replace('tchalla-fase3.html');
         } else {
             alert("Que pena, você errou. Tente novamente.");
             var respostaJogador = prompt("Digite a resposta");
             if (respostaJogador.toUpperCase() === respostaCerta) {
                 alert("Parabéns! Você acertou na terceira tentativa! Ganhou 10xp");
-                window.location.replace('tchalla-fase2.html');
+                window.location.replace('tchalla-fase3.html');
             } else {
                 alert("Que pena, você errou pela terceira vez. GAME OVER");
                 window.location.replace('../game-over.html');
---
+++ tchalla/tchalla-fase3.js
@@ -0,0 +1,19 @@
+function confereResposta() {
+    var respostaJogador = prompt("Digite a resposta");
+    var respostaCerta = "B";
+    if (respostaJogador.toUpperCase() === respostaCerta) {
+        alert("Parabéns! Você foi estrategista ao tentar evitar uma luta desnecessária e demonstrou habilidades de negociação, fundamentais para um líder. Ganhou 50xp");
+        window.location.replace('tchalla-fase4.html');
+    } else if (respostaJogador.toUpperCase() === "A") {
+        alert("A princípio pode parecer uma boa ideia, mas essa escolha provocou um tumulto no local e você não conseguiu pegar o artefato! Volte à fase II e ouça seu pai novamente");
+        window.location.replace('tchalla-fase2.html');
+    } else if (respostaJogador.toUpperCase() === "C") {
+        alert("Você escolheu lutar com Klaue! Corajoso, porém, o vilão conseguiu fugir. Você e sua equipe ainda fazem uma grande perseguição, mas Klaue escapa. Pelo menos vocês resgataram o artefato. Ganhou 20xp");
+        window.location.replace('tchalla-fase4.html');
+    } else if (respostaJogador.toUpperCase() === "D") {
+        alert("Que escolha lamentável! Não é o que se espera de um Rei como T'Challa. GAME OVER!");
+        window.location.replace('../game-over.html');
+    } else {
+        alert("Resposta inválida");
+    }
+}
