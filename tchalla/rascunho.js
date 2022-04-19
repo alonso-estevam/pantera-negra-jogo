54f655effa42bab778aa7c7dff42c79ea30c7db2
alonso-estevam <alonso_estevam@outlook.com>
2022-04-19 16:43:16 -0300

    desenvolvi a função para conferir respostas e adicionei página do game over

--- tchalla/rascunho.js
+++ tchalla/rascunho.js
@@ -6,21 +6,46 @@
 5 - permitir até 4 tentativas;
 */
 
-function conferirPalpite () {
-    var numeroAleatorio = Math.floor(Math.random() * 40) + 1;
-    var palpite = Number(prompt("Digite seu palpite"));
+// function conferirPalpite () {
+//     var numeroAleatorio = Math.floor(Math.random() * 40) + 1;
+//     var palpite = Number(prompt("Digite seu palpite"));
     
-    for (var i = 0; i < 4; i++) {
-        if (palpite === numeroAleatorio) {
-            alert("Excelente! Você acertou o número!");
-            document.write(`<p>Ir para a próxima fase</p>`)
-        } else if (palpite > numeroAleatorio) {
-            alert("Que pena! Seu palpite foi acima do número");
-            var palpite = Number(prompt("Digite um novo palpite"));
-        } else if (palpite < numeroAleatorio) {
-            alert("Que pena! Seu palpite foi abaixo do número");
-            var palpite = Number(prompt("Digite um novo palpite"));
-        }
-    }
+//     for (var i = 0; i < 4; i++) {
+//         if (palpite === numeroAleatorio) {
+//             alert("Excelente! Você acertou o número!");
+//             document.write(`<p>Ir para a próxima fase</p>`)
+//         } else if (palpite > numeroAleatorio) {
+//             alert("Que pena! Seu palpite foi acima do número");
+//             var palpite = Number(prompt("Digite um novo palpite"));
+//         } else if (palpite < numeroAleatorio) {
+//             alert("Que pena! Seu palpite foi abaixo do número");
+//             var palpite = Number(prompt("Digite um novo palpite"));
+//         }
+//     }
     
+// }
+
+// OUTRA TENTATIVA DO DESAFIO ADIVINHE O NÚMERO
+// Segundo o MDN, "a função Math.random() retorna um número pseudo-aleatório no intervalo [0, 1[, ou seja, de 0 (inclusivo) até, mas não incluindo, 1 (exclusivo), que depois você pode dimensionar para um intervalo desejado."
+//var respostaCerta = Math.round(Math.random()*10);
+
+// Para incluir qualquer intervalo de valores diferentes de base 10:
+
+function geraNumeroAleatorio(min,max) {
+    min = Math.ceil(min); // "Arredonda" pra cima
+    max = Math.floor(max); // "Arredonda" pra baixo
+    return Math.floor(Math.random()*(max - min + 1) + min); // Desse modo para incluir o maior valor
 }
+
+var respostaCerta = geraNumeroAleatorio(1,40);
+
+var respostaJogador = parseInt(prompt("Digite seu palpite: "));
+
+if (respostaJogador === respostaCerta) {
+    alert("Você acertou!");
+} else if (respostaJogador > respostaCerta) {
+        alert("Número muito alto. Você errou!");
+        
+    } else if (respostaJogador < respostaCerta) {
+        alert("Número muito baixo. Você errou!")
+    }
--- tchalla/tchalla-fase1.js
+++ tchalla/tchalla-fase1.js
@@ -1,11 +1,22 @@
-
-
-function confere () {
+function confereResposta() {
     var respostaJogador = prompt("Digite a resposta");
     var respostaCerta = "C";
-    if (respostaJogador === respostaCerta) {
-        alert("Parabéns! Você acertou!");
+    if (respostaJogador.toUpperCase() === respostaCerta) {
+        alert("Parabéns! Você acertou na primeira tentativa! Ganhou 50xp");
     } else {
-        alert("Que pena, você errou");
+        alert("Que pena, você errou. Tente novamente.");
+        var respostaJogador = prompt("Digite a resposta");
+        if (respostaJogador.toUpperCase() === respostaCerta) {
+            alert("Parabéns! Você acertou na segunda tentativa! Ganhou 30xp");
+        } else {
+            alert("Que pena, você errou. Tente novamente.");
+            var respostaJogador = prompt("Digite a resposta");
+            if (respostaJogador.toUpperCase() === respostaCerta) {
+                alert("Parabéns! Você acertou na terceira tentativa! Ganhou 10xp");
+            } else {
+                alert("Que pena, você errou pela terceira vez. GAME OVER");
+                window.location.replace('../game-over.html')
+            }
+        } 
     }
 }
