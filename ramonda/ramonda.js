8ab8a161793012e53df781d2ffba11de06a058d6
alonso-estevam <alonso_estevam@outlook.com>
2022-04-22 00:57:30 -0300

    ajustes na ambientação

--- ramonda/ramonda.js
+++ ramonda/ramonda.js
@@ -11,8 +11,8 @@ function confereResposta1(respostaCerta) {
             break;
         } else if (respostaJogador.toUpperCase() === 'A' ||  respostaJogador.toUpperCase() === 'B' || respostaJogador.toUpperCase() === 'C') {
             contador--;
-            alert(`Resposta incorreta, tente novamente!\n Você tem ${contador} tentativas!`);
-            escolha = prompt('Digite a resposta (A, B, C ou D):')
+            alert(`Resposta incorreta, tente novamente!\n Tentativas restantes: ${contador}`);
+            respostaJogador = prompt('Digite a resposta (A, B, C ou D):')
         } else {
             alert("Resposta inválida! Digite A, B, C ou D")
             respostaJogador = prompt('Digite a resposta (A, B, C ou D):');
@@ -33,8 +33,8 @@ function confereResposta2(respostaCerta) {
             break;
         } else if (respostaJogador.toUpperCase() === 'A' ||  respostaJogador.toUpperCase() === 'C' || respostaJogador.toUpperCase() === 'D') {
             contador--;
-            alert(`Resposta incorreta, tente novamente!\n Você tem ${contador} tentativas!`);
-            escolha = prompt('Digite a resposta (A, B, C ou D):')
+            alert(`Resposta incorreta, tente novamente!\n Tentativas restantes: ${contador}`);
+            respostaJogador = prompt('Digite a resposta (A, B, C ou D):')
         } else {
             alert("Resposta inválida! Digite A, B, C ou D")
             respostaJogador = prompt('Digite a resposta (A, B, C ou D):');
@@ -55,7 +55,7 @@ function confereResposta3(respostaCerta) {
             break;
         } else if (respostaJogador.toUpperCase() === 'B' ||  respostaJogador.toUpperCase() === 'C' || respostaJogador.toUpperCase() === 'D') {
             contador--;
-            alert(`Resposta incorreta, tente novamente!\n Você tem ${contador} tentativas!`);
+            alert(`Resposta incorreta, tente novamente!\n Tentativas restantes: ${contador}`);
             escolha = prompt('Digite a resposta (A, B, C ou D):')
         } else {
             alert("Resposta inválida! Digite A, B, C ou D")
--- shuri/shuri-fases.js
+++ shuri/shuri-fases.js
@@ -12,7 +12,7 @@ function confereResposta1(respostaCerta) {
         } else if (respostaJogador.toUpperCase() === 'B' ||  respostaJogador.toUpperCase() === 'C' || respostaJogador.toUpperCase() === 'D') {
             contador--;
             alert(`Resposta incorreta, tente novamente!\n Você tem ${contador} tentativas!`);
-            escolha = prompt('Digite a resposta (A, B, C ou D):');
+            respostaJogador = prompt('Digite a resposta (A, B, C ou D):');
         } else {
             alert("Resposta inválida! Digite A, B, C ou D")
             respostaJogador = prompt('Digite a resposta (A, B, C ou D):');
@@ -35,7 +35,7 @@ function confereResposta2(respostaCerta) {
         } else if (respostaJogador.toUpperCase() === 'A' ||  respostaJogador.toUpperCase() === 'B' || respostaJogador.toUpperCase() === 'C') {
             contador--;
             alert(`Resposta incorreta, tente novamente!\n Você tem ${contador} tentativas!`);
-            escolha = prompt('Digite a resposta (A, B, C ou D):')
+            respostaJogador = prompt('Digite a resposta (A, B, C ou D):')
         } else {
             alert("Resposta inválida! Digite A, B, C ou D")
             respostaJogador = prompt('Digite a resposta (A, B, C ou D):');
@@ -58,7 +58,7 @@ function confereResposta3(respostaCerta) {
         } else if (respostaJogador.toUpperCase() === 'A' ||  respostaJogador.toUpperCase() === 'B' || respostaJogador.toUpperCase() === 'D') {
             contador--;
             alert(`Resposta incorreta, tente novamente!\n Você tem ${contador} tentativas!`);
-            escolha = prompt('Digite a alternativa escolhida através da letra:')
+            respostaJogador = prompt('Digite a alternativa escolhida através da letra:')
         } else {
             alert("Resposta inválida! Digite A, B, C ou D")
             respostaJogador = prompt('Digite a resposta (A, B, C ou D):');
